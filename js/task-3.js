e5b8ec45cf56daf6491d04a8714033e9edc2fd58
parfinna <143504651+parfinna@users.noreply.github.com>
2024-01-27 19:34:49 +0200

    homeworkJS-3

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+    const filteredArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            filteredArray.push(numbers[i]);
+        }
+    }
+        return filteredArray;
+}
+// шось ця задачка мені далась туго(( 
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5)); 
+console.log(filterArray([12, 24, 8, 41, 76], 38)); 
+console.log(filterArray([12, 24, 8, 41, 76], 20));
